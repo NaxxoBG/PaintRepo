a42d95d5d3812b78abf9fd5bfda1fc730550ba1c
Atanas Latinov <atanas.latinov@yahoo.com>
2019-03-13 21:34:07 +0100

    Revision
    - small changes to reduce the number of IDE warnings,
    - listed a couple of files to not be tracked by git
    - added a validator regex
    - changed the ParseToString Function to use the validator
    - a small change to one of the tests so it works on Windows machines

--- src/main/scala/DrawingEngine.sc
+++ src/main/scala/DrawingEngine.sc
@@ -1,13 +1,13 @@
 
-sealed abstract class Figure;
-case class Line(x1: Int, y1: Int, x2: Int, y2: Int)  extends Figure;
-case class Circle(x: Int, y: Int, r: Int) extends Figure;
-case class TextAt(x: Int, y: Int, t: String) extends Figure;
-case class Rectangle(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure;
-case class Draw(c: String, list: List[Figure]) extends Figure;
-case class BoundingBox(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure;
-case class Fill(c: String, figure: Figure) extends Figure;
-case class Error() extends Figure;
+sealed abstract class Figure
+case class Line(x1: Int, y1: Int, x2: Int, y2: Int)  extends Figure
+case class Circle(x: Int, y: Int, r: Int) extends Figure
+case class TextAt(x: Int, y: Int, t: String) extends Figure
+case class Rectangle(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure
+case class Draw(c: String, list: List[Figure]) extends Figure
+case class BoundingBox(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure
+case class Fill(c: String, figure: Figure) extends Figure
+case class Error() extends Figure
 
 
 val LineRegex = "\\((?i)Line\\((\\d+) (\\d+)\\) \\((\\d+) (\\d+)\\)\\)".r
@@ -17,6 +17,8 @@ val TextAtRegex = "\\((?i)Text-At\\((\\d+) (\\d+)\\) (.*)\\)".r
 val DrawRegex = "\\((?i)Draw\\(([a-zA-Z]*) (.*)\\)\\)".r
 val BoundingBoxRegex = "\\((?i)Bounding-Box\\((\\d+) (\\d+)\\) \\((\\d+) (\\d+)\\)\\)".r
 val FillRegex = "\\((?i)Fill\\(([a-zA-Z]*) (.*)\\)\\)".r
+
+val validator = "(?i)(\\([a-z]+(?:\\-[a-z]+)?\\(\\d+ \\d+\\)(?: (?:\\d|[a-z ]+|\\(\\d+ \\d+\\)))?\\))".r
 
 val LineCommand = "(LinE(2 3) (4 5))"
 val CircleCommand = "(Circle(1 2) 2)"
@@ -42,8 +44,8 @@ def ParseFromString(command: String): Figure  = command match {
   case BoundingBoxRegex(AsInt(x1), AsInt(y1), AsInt(x2), AsInt(y2)) => BoundingBox(x1, y1, x2, y2)
   case TextAtRegex(AsInt(x), AsInt(y), t) => TextAt(x, y, t)
   case FillRegex(c, figure) => Fill(c, ParseFromString(figure))
-  case DrawRegex(c, t) => Draw(c, t.split("\\) (" ).map(cmd => ParseFromString(cmd)).toList)
-  case _ => throw new NoSuchMethodException// could not parse command
+  case DrawRegex(c, t) => Draw(c, validator.findAllIn(t).map(cmd => ParseFromString(cmd)).toList)
+  case _ => throw new Exception("Could not parse command") // could not parse command
 }
 
 val l = ParseFromString(LineCommand)
@@ -79,7 +81,7 @@ val tree = generateAbstractSyntaxTree(builder.toString())
 // A valid syntax is also one with no parsing errors
 def IsSyntaxTreeValid(array: List[Figure]): Boolean =
   array.collect{case fig:BoundingBox => fig}.length == 1 &&
-    array.collect{case fig:Error => fig}.length == 0
+    array.collect { case fig: Error => fig }.isEmpty
 
 val isValid = IsSyntaxTreeValid(tree)
 
--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,30 +1,33 @@
+import scala.util.matching.Regex
+
 object Parser {
 
-  sealed abstract class Figure;
-  case class Line(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure;
-  case class Circle(x: Int, y: Int, r: Int) extends Figure;
-  case class TextAt(x: Int, y: Int, t: String) extends Figure;
-  case class Rectangle(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure;
-  case class Draw(c: String, list: List[Figure]) extends Figure;
-  case class BoundingBox(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure;
-  case class Fill(c: String, figure: Figure) extends Figure;
-  case class Nil() extends Figure;
-  case class Error() extends Figure;
+  sealed abstract class Figure
+  case class Line(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure
+  case class Circle(x: Int, y: Int, r: Int) extends Figure
+  case class TextAt(x: Int, y: Int, t: String) extends Figure
+  case class Rectangle(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure
+  case class Draw(c: String, list: List[Figure]) extends Figure
+  case class BoundingBox(x1: Int, y1: Int, x2: Int, y2: Int) extends Figure
+  case class Fill(c: String, figure: Figure) extends Figure
+  case class Nil() extends Figure
+  case class Error() extends Figure
 
-  val LineRegex = "\\((?i)Line\\((\\d+) (\\d+)\\) \\((\\d+) (\\d+)\\)\\)".r
-  val CircleRegex = "\\((?i)Circle\\((\\d+) (\\d+)\\) (\\d+)\\)".r
-  val RectangleRegex = "\\((?i)Rectangle\\((\\d+) (\\d+)\\) \\((\\d+) (\\d+)\\)\\)".r
-  val TextAtRegex = "\\((?i)Text-At\\((\\d+) (\\d+)\\) (.*)\\)".r
-  val DrawRegex = "\\((?i)Draw\\(([a-zA-Z]*) (.*)\\)\\)".r
-  val BoundingBoxRegex = "\\((?i)Bounding-Box\\((\\d+) (\\d+)\\) \\((\\d+) (\\d+)\\)\\)".r
-  val FillRegex = "\\((?i)Fill\\(([a-zA-Z]*) (.*)\\)\\)".r
+  val LineRegex: Regex = "\\((?i)Line\\((\\d+) (\\d+)\\) \\((\\d+) (\\d+)\\)\\)".r
+  val CircleRegex: Regex = "\\((?i)Circle\\((\\d+) (\\d+)\\) (\\d+)\\)".r
+  val RectangleRegex: Regex = "\\((?i)Rectangle\\((\\d+) (\\d+)\\) \\((\\d+) (\\d+)\\)\\)".r
+  val TextAtRegex: Regex = "\\((?i)Text-At\\((\\d+) (\\d+)\\) (.*)\\)".r
+  val DrawRegex: Regex = "\\((?i)Draw\\(([a-zA-Z]*) (.*)\\)\\)".r
+  val BoundingBoxRegex: Regex = "\\((?i)Bounding-Box\\((\\d+) (\\d+)\\) \\((\\d+) (\\d+)\\)\\)".r
+  val FillRegex: Regex = "\\((?i)Fill\\(([a-zA-Z]*) (.*)\\)\\)".r
+  val validator: Regex = "(?i)(\\([a-z]+(?:\\-[a-z]+)?\\(\\d+ \\d+\\)(?: (?:\\d|[a-z ]+|\\(\\d+ \\d+\\)))?\\))".r
 
 
   object AsInt {
-    def unapply(s: String) = try {
+    def unapply(s: String): Option[Int] = try {
       Some(s.toInt)
     } catch {
-      case e: NumberFormatException => None
+      case _: NumberFormatException => None
     }
   }
 
@@ -36,13 +39,13 @@ object Parser {
     case BoundingBoxRegex(AsInt(x1), AsInt(y1), AsInt(x2), AsInt(y2)) => BoundingBox(x1, y1, x2, y2)
     case TextAtRegex(AsInt(x), AsInt(y), t) => TextAt(x, y, t)
     case FillRegex(c, figure) => Fill(c, ParseFromString(figure))
-    case DrawRegex(c, t) => Draw(c, t.split(" \\(" ).map(cmd => ParseFromString(cmd)).toList)
+    case DrawRegex(c, t) => Draw(c, validator.findAllIn(t).map(cmd => ParseFromString(cmd)).toList)
     case _ => throw new NoSuchMethodException // could not parse command
   }
 
 
   def generateAbstractSyntaxTree(commands: String): List[Figure] = {
-    commands.split("\\n").map(cmd => ParseFromString(cmd)).toList
+    commands.split("\\r?\\n").map(cmd => ParseFromString(cmd)).toList
   }
 
 
@@ -50,12 +53,12 @@ object Parser {
   // A valid syntax is also one with no parsing errors
   def IsSyntaxTreeValid(array: List[Figure]): Boolean =
     array.collect { case fig: BoundingBox => fig }.length == 1 &&
-      array.collect { case fig: Error => fig }.length == 0
+      array.collect { case fig: Error => fig }.isEmpty
 
 
 
   def DrawSyntaxTree(commands: String): Unit = {
-    val syntaxTree = generateAbstractSyntaxTree(commands);
+    val syntaxTree = generateAbstractSyntaxTree(commands)
     if(IsSyntaxTreeValid(syntaxTree)){
 
     }
